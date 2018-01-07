72d020081547301169fa326402d7676fc1a7236f
Dolan <dolan_miu@hotmail.com>
2018-01-07 23:03:20 +0000

    Add single pump scripts

---
+++ single-pump/pump-water-in.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+################################# DC motor test!
+myMotor = mh.getMotor(1)
+
+# set the speed to start, from 0 (off) to 255 (max speed)
+myMotor.setSpeed(200)
+myMotor.run(Adafruit_MotorHAT.FORWARD);
+# turn on motor
+time.sleep(1006)
+myMotor.run(Adafruit_MotorHAT.RELEASE);
